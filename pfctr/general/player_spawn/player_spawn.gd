d939924e031924047d4205f15588012ce7186607
rgabrielfs <rgabrielfialho@gmail.com>
2026-09-11 16:04:37 -0300

    Added player spawner and fixed sky scene

---
+++ pfctr/general/player_spawn/player_spawn.gd
@@ -0,0 +1,18 @@
+class_name PlayerSpawn extends Node2D
+
+const PLAYER = preload("uid://be2p5bjigf8yd")
+
+func _ready() -> void:
+	visible = false
+	await get_tree().process_frame
+	
+	if get_tree().get_first_node_in_group("Player"):
+		print("there is player")
+		return
+	print("No player")
+
+	var player: Player = load("uid://be2p5bjigf8yd").instantiate()
+	get_tree().root.add_child( player )
+
+	player.global_position = self.global_position
+	pass 
