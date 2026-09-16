0ec0f36381ac0e935e6b013b1b99d8b9adbaa980
rgabrielfs <rgabrielfialho@gmail.com>
2026-09-16 00:24:32 -0300

    adjusted camera smoothing

--- pfctr/title/title_screen.gd
+++ pfctr/title/title_screen.gd
@@ -1,1 +1,20 @@
 extends CanvasLayer
+
+@onready var main_menu: VBoxContainer = %MainMenu
+@onready var new_game_menu: VBoxContainer = %NewGameMenu
+@onready var load_game_menu: VBoxContainer = %LoadGameMenu
+
+@onready var new_game: Button = %NewGame
+@onready var load_game: Button = %LoadGame
+
+@onready var new_slot: Button = %NewSlot
+@onready var new_slot_2: Button = %NewSlot2
+@onready var new_slot_3: Button = %NewSlot3
+
+@onready var load_slot: Button = %LoadSlot
+@onready var load_slot_2: Button = %LoadSlot2
+@onready var load_slot_3: Button = %LoadSlot3
+
+func _ready() -> void:
+	
+	pass
